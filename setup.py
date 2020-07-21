701226f813d9cdda7435886027029711fa7e0ed7
Markus Monz <markusmonz@googlemail.com>
2020-07-21 10:02:36 +0200

    init project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Image classification to classify usable images for time lapse video.',
+    author='Markus Monz',
+    license='MIT',
+)
